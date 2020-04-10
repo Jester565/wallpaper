bcf85c93cacd81379ca688ac49d8069bd492b81a
Jester565 <ajcraig543@gmail.com>
2020-04-10 10:25:06 -0700

    Path to executable calculated at run-time

--- source/win.js
+++ source/win.js
@@ -6,14 +6,19 @@ const childProcess = require('child_process');
 const execFile = promisify(childProcess.execFile);
 
 // Binary source → https://github.com/sindresorhus/win-wallpaper
-const binary = path.join(__dirname, 'win-wallpaper.exe');
+
+const getBinary = () => {
+	return path.join(path.resolve(), 'node_modules/wallpaper/source/win-wallpaper.exe');
+}
 
 exports.get = async () => {
+	const binary = getBinary();
 	const {stdout} = await execFile(binary);
 	return stdout.trim();
 };
 
 exports.set = async imagePath => {
+	const binary = getBinary();
 	if (typeof imagePath !== 'string') {
 		throw new TypeError('Expected a string');
 	}
